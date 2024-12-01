0695c557626bdcb60c452a15e413ebb0cfe097ce
Esteban Ramos <estebanramo@gmail.com>
2024-12-01 03:57:55 +0000

    add PR_SET_TIMERSLACK to benchmark

--- bench_test.go
+++ bench_test.go
@@ -2,7 +2,9 @@ package notnets_grpc
 
 import (
 	"log"
+	"syscall"
 	"testing"
+	"time"
 
 	test_hello_service "github.com/EIRNf/notnets_grpc/test_hello_service"
 	"github.com/rs/zerolog"
@@ -10,9 +12,12 @@ import (
 
 func BenchmarkGrpcOverSharedMemory(b *testing.B) {
 
-	// debug.SetGCPercent(1000)
+	// debug.SetGCPercent(-1)
 	// runtime.MemProfileRate = 1
 
+	syscall.Syscall(syscall.SYS_PRCTL, syscall.PR_SET_TIMERSLACK, 1, 0)
+	// syscall.Syscall(syscall.SYS_SCHED_SETSCHEDULER, syscall.SCHED_FIFO, 0, 0)
+	
 
 	zerolog.SetGlobalLevel(zerolog.InfoLevel)
 
@@ -31,6 +36,8 @@ func BenchmarkGrpcOverSharedMemory(b *testing.B) {
 	if err != nil {
 		log.Fatalf("did not connect: %v", err)
 	}
+	time.Sleep(5 * time.Second)
+
 
 	// grpchantesting.RunChannelTestCases(t, &cc, true)
 	test_hello_service.RunChannelBenchmarkCases(b, cc, false)
